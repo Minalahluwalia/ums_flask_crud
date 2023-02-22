24a9974784a3a8e088535926e487ff7438af2283
Minal Ahluwalia <41380999+Minalahluwalia@users.noreply.github.com>
2023-02-22 20:54:10 +0530

    Update mydata.sql

--- mydata.sql
+++ mydata.sql
@@ -2,40 +2,41 @@ show databases;
 
 USE `mydata`;
 
-CREATE TABLE `user` (
-  `userid` int NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` enum('admin','user') NOT NULL,
-  `country` varchar(350) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userid`);
+CREATE TABLE user (
+  userid INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255),
+  role ENUM('admin', 'user'),
+  country varchar(350)
+) ENGINE=InnoDB CHARSET=utf8mb4;
 
-ALTER TABLE `user`
-  MODIFY `userid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;  
-  
-INSERT INTO user VALUES(1,"xyz", "xyzS12@gmail.com","xy123","user","India"); 
 
-SELECT * FROM user WHERE email = "xyzS12@gmail.com" AND password = "xy123";
+INSERT INTO user (name, email, password, role, country) VALUES ('xyz', 'xy123@example.com', 'xyz12', 'admin', 'USA');
+ 
 
-SELECT * FROM `user`;
+SELECT * FROM user WHERE email = "xy123@example.com" AND password = "xyz12";
 
-UPDATE `user` SET role ="user", country ="India" WHERE userid = "7";
+desc user;
 
-UPDATE `user` SET role = "admin" WHERE userid = "7";
+UPDATE user SET role ="user", country ="India" WHERE userid = "";
 
-DELETE FROM `user` WHERE email = "xyzS12@gmail.com";
+UPDATE `user` SET role = "admin" WHERE userid = "1";
 
-DELETE FROM user WHERE email = "xyzS12@gmail.com" AND userid = "1";
+DELETE FROM user WHERE userid = 1;
+
+DELETE FROM user WHERE userid = "3";
 
 SELECT name, userid, role, email
 FROM user;
 
-UPDATE `user` SET name ="Aarti", role ="user", country ="India" WHERE userid = 4;
+SELECT * FROM user WHERE userid = "2";
 
-INSERT INTO `user` (`userid`,`name`, `email`,`password`,`role`,`country`) VALUES (4,"abc","xyz@gmail.com","xyz","user","user");
+UPDATE user SET name ="mickey", role ="user", country ="India" WHERE userid = 2;
+
+SELECT * FROM user;
+
+UPDATE user SET name ="Aarti", role ="user", country ="India" WHERE userid = 4;
 
 DELETE FROM user WHERE userid = "5";
